c51dace510fa6ca67f68cd3f573bc1fc2c5df9dc
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-07-07 12:37:03 -0500

    electron commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+import{Beer} from './Beer';
+
+
+export default class App extends React.Component {
+    // the constructor for the props of the elements
+    constructor(props) {
+      super(props);
+    }
+
+}    
+
+
+
+    return(
+        <View>
+            <beer>
+                <li>IPAD Beer</li>
+            </beer>
+
+        </View>
+    )
---
+++ Beer.js
@@ -0,0 +1,31 @@
+import react from 'react';
+import { StyleSheet, Text, View, Image,ImageBackground,paragraph,Dimensions, SafeAreaView, ScrollView  } from 'react-native';
+
+const beer =() => {
+    beer = [victory,corona,IPA];
+    
+}
+
+return (
+    <View>
+         <img style={{color: '#fff',fontSize:40,flex: .5,alignContent:'center'}}
+          src="image/coronabeer.jpg"
+          style="float: right; width: 42px; height: 42px;"
+          alt="GOOD"
+        />
+    </View>
+   
+)
+
+
+
+   
+  
+
+
+
+
+
+    
+
+
